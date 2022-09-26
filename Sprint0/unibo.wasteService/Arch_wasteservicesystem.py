326628ea8d55adb40707b13693c8dd205181f7e5
Enrico Valastro <enrico.valastro@studio.unibo.it>
2022-09-26 15:45:00 +0200

    Sprint0_Review

--- Sprint0/unibo.wasteService/Arch_wasteservicesystem.py
+++ Sprint0/unibo.wasteService/Arch_wasteservicesystem.py
@@ -18,7 +18,7 @@ eventedgeattr = {
 }
 with Diagram('wasteservicesystemArch', show=False, outformat='png', graph_attr=graphattr) as diag:
   with Cluster('ctxtruck', graph_attr=nodeattr):
-          smartdevice=Custom('smartdevice','./qakicons/symActorSmall.png')  
+          smartdevice=Custom('smartdevice','./qakicons/symActorSmall.png')
   with Cluster('env'):
      sys = Custom('','./qakicons/system.png')
      with Cluster('ctxpi', graph_attr=nodeattr):
@@ -32,19 +32,19 @@ with Diagram('wasteservicesystemArch', show=False, outformat='png', graph_attr=g
      with Cluster('ctxwasteservice', graph_attr=nodeattr):
           wasteservice=Custom('wasteservice','./qakicons/symActorSmall.png')
           transporttrolley=Custom('transporttrolley','./qakicons/symActorSmall.png')
-     wasteservice >> Edge(color='blue', style='solid', xlabel='pickingup', fontcolor='blue') >> transporttrolley
-     wasteservice >> Edge(color='blue', style='solid', xlabel='droppingout', fontcolor='blue') >> transporttrolley
-     wasteservice >> Edge(color='blue', style='solid', xlabel='backhome', fontcolor='blue') >> transporttrolley
+     wasteservice >> Edge(color='magenta', style='solid', xlabel='pickup', fontcolor='magenta') >> transporttrolley
+     wasteservice >> Edge(color='magenta', style='solid', xlabel='droppout', fontcolor='magenta') >> transporttrolley
      transporttrolley >> Edge(color='blue', style='solid', xlabel='turnOn', fontcolor='blue') >> led
      transporttrolley >> Edge(color='blue', style='solid', xlabel='turnOff', fontcolor='blue') >> led
      transporttrolley >> Edge(color='blue', style='solid', xlabel='blink', fontcolor='blue') >> led
      transporttrolley >> Edge(color='blue', style='solid', xlabel='cmd', fontcolor='blue') >> basicrobot
      transporttrolley >> Edge(color='magenta', style='solid', xlabel='step', fontcolor='magenta') >> basicrobot
-     sonar >> Edge( xlabel='obstacle', **eventedgeattr, fontcolor='red') >> sys
-     sonar >> Edge( xlabel='sonar', **eventedgeattr, fontcolor='red') >> sys
      smartdevice >> Edge(color='magenta', style='solid', xlabel='storeWaste', fontcolor='magenta') >> wasteservice
-     wasteservice >> Edge(color='darkgreen', style='dashed', xlabel='loadaccept', fontcolor='darkgreen') >> smartdevice
-     wasteservice >> Edge(color='darkgreen', style='dashed', xlabel='loadrejected', fontcolor='darkgreen') >> smartdevice
+     transporttrolley >> Edge(color='darkgreen', style='dashed', xlabel='pickupdone', fontcolor='darkgreen') >> wasteservice
+     transporttrolley >> Edge(color='darkgreen', style='dashed', xlabel='dropoutdone', fontcolor='darkgreen') >> wasteservice
      basicrobot >> Edge(color='darkgreen', style='dashed', xlabel='stepdone', fontcolor='darkgreen') >> transporttrolley
      basicrobot >> Edge(color='darkgreen', style='dashed', xlabel='stepfail', fontcolor='darkgreen') >> transporttrolley
+     wasteservice >> Edge(color='darkgreen', style='dashed', xlabel='loadaccept', fontcolor='darkgreen') >> smartdevice
+     wasteservice >> Edge(color='darkgreen', style='dashed', xlabel='loadrejected', fontcolor='darkgreen') >> smartdevice
+
 diag
