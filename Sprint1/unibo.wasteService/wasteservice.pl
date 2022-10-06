82590295182367ffd46572f9f684e892a15a47b7
annamaria <annamaria.simonitto@studio.unibo.it>
2022-10-06 17:37:44 +0200

    Sprint1_Project1.0

---
+++ Sprint1/unibo.wasteService/wasteservice.pl
@@ -0,0 +1,5 @@
+%====================================================================================
+% wasteservice description   
+%====================================================================================
+context(ctxwasteservice, "localhost",  "TCP", "8055").
+ qactor( containermanager, ctxwasteservice, "it.unibo.containermanager.Containermanager").
