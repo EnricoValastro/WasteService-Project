3b42f5449787182ba7428a3eedc4eb7b7fa8273b
Enrico Valastro <enrico.valastro@studio.unibo.it>
2022-10-10 10:57:50 +0200

    Sprint1_Project2.1

--- Sprint1/unibo.transportTrolley/Arch_transporttrolley.py
+++ Sprint1/unibo.transportTrolley/Arch_transporttrolley.py
@@ -23,6 +23,12 @@ with Diagram('transporttrolleyArch', show=False, outformat='png', graph_attr=gra
           transporttrolleycore=Custom('transporttrolleycore','./qakicons/symActorSmall.png')
           transporttrolleymover=Custom('transporttrolleymover','./qakicons/symActorSmall.png')
           transporttrolleyexecutor=Custom('transporttrolleyexecutor','./qakicons/symActorSmall.png')
+     with Cluster('ctxwasteservice', graph_attr=nodeattr):
+          wasteservicecore=Custom('wasteservicecore(ext)','./qakicons/externalQActor.png')
+     with Cluster('ctxbasicrobot', graph_attr=nodeattr):
+          pathexec=Custom('pathexec(ext)','./qakicons/externalQActor.png')
      transporttrolleycore >> Edge(color='magenta', style='solid', xlabel='moveto', fontcolor='magenta') >> transporttrolleymover
      transporttrolleycore >> Edge(color='magenta', style='solid', xlabel='execaction', fontcolor='magenta') >> transporttrolleyexecutor
+     transporttrolleycore >> Edge(color='blue', style='solid', xlabel='dropoutdone', fontcolor='blue') >> wasteservicecore
+     transporttrolleymover >> Edge(color='magenta', style='solid', xlabel='dopath', fontcolor='magenta') >> pathexec
 diag
---
+++ Sprint1/unibo.transportTrolley/resources/utility/ServiceAreaConfig.kt
@@ -0,0 +1,22 @@
+package utility
+
+import com.google.common.io.Resources
+import org.json.JSONObject
+import java.io.File
+import java.nio.charset.StandardCharsets
+
+class ServiceAreaConfig {
+
+    companion object {
+        val config = File("wasteServiceSystemConfig.json").readText(StandardCharsets.UTF_8)
+        val JsonObject = JSONObject(config)
+        val LocJsonObj = JSONObject(JsonObject.get("LOCATION").toString())
+
+        fun getXDestination(pos : String) : Int{
+            return LocJsonObj.getJSONArray(pos).get(0).toString().toInt()
+        }
+        fun getYDestination(pos : String) : Int{
+            return LocJsonObj.getJSONArray(pos).get(1).toString().toInt()
+        }
+    }
+}
