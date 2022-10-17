b64e310c19be93535d5f872f15c7770ed63b921a
Alessio Reitano <alessio.reitano@studio.unibo.it>
2022-10-17 20:36:43 +0200

    Sprint2_webgui1.1

---
+++ Sprint2/webgui/src/main/resources/static/app.js
@@ -0,0 +1,11 @@
+/*function change() {
+
+    if (document.getElementById("plasticprog").textContent != '...' || document.getElementById("glassprog").textContent != '...') {
+
+        let plastic = Number(document.getElementById("plasticprog").textContent);
+
+        let glass = Number(document.getElementById("glassprog").textContent);
+
+    }
+
+}*/
