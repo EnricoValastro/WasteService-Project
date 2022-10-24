2228b2c553bd7f87cb520ba14531591f8dfd75d2
Enrico Valastro <enrico.valastro@studio.unibo.it>
2022-10-24 16:30:11 +0200

    Sprint1_someFix

--- Sprint1/userDocs/resources/kotlin/ServiceAreaDestinationConfig.kt
+++ Sprint1/userDocs/resources/kotlin/ServiceAreaDestinationConfig.kt
@@ -7,6 +7,13 @@ import java.nio.charset.StandardCharsets
 class ServiceAreaDestinationConfig {
 
     companion object {
+        val DIR_TO_PLAN = mapOf<Int, String>(
+                0 to "rightDir",
+                90 to "downDir",
+                180 to "leftDir",
+                270 to "upDir"
+        )
+
         val config = File("wasteServiceSystemConfig.json").readText(StandardCharsets.UTF_8)
         val JsonObject = JSONObject(config)
         val LocJsonObj = JSONObject(JsonObject.get("LOCATION").toString())
@@ -17,5 +24,15 @@ class ServiceAreaDestinationConfig {
         fun getYDestination(pos : String) : Int{
             return LocJsonObj.getJSONArray(pos).get(1).toString().toInt()
         }
+
+        fun getDirection(pos : String) : Int{
+            return LocJsonObj.getJSONArray(pos).get(2).toString().toInt()
+        }
+
+        fun getPlannerDirection(pos : String) : String{
+            return DIR_TO_PLAN[getDirection(pos)]!!
+        }
+
+
     }
 }
