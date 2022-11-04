6492e14353685c65ec5bf3a68af6a2d4685e0efd
Alessio Reitano <alessio.reitano@studio.unibo.it>
2022-11-04 17:14:09 +0100

    Sprint2_webgui_final

---
+++ Sprint2/unibo.webgui/src/main/resources/static/minimal.js
@@ -0,0 +1,77 @@
+/*
+minimal.js
+*/
+
+var socket;
+
+function connect() {
+    var host = document.location.host;
+    var pathname = "/"                   //document.location.pathname;
+    var addr = "ws://" + host + pathname + "socket";
+    // Assicura che sia aperta un unica connessione
+    if (socket !== undefined && socket.readyState !== WebSocket.CLOSED) {
+        alert("WARNING: Connessione WebSocket già stabilita");
+    }
+    socket = new WebSocket(addr);
+
+    function sendMessage(message) {
+        var jsonMsg = JSON.stringify({ 'name': message });
+        socket.send(jsonMsg);
+        console.log("Sent Message: " + jsonMsg);
+    }
+
+    socket.onmessage = function (event) {
+        //alert(`Got Message: ${event.data}`);
+        msg = event.data;
+
+        //alert(`Got Message: ${msg}`);
+        console.log("ws-status:" + msg);
+
+        let container = JSON.parse(msg);
+        let pCurr = container['boxCurrentWeight']['PLASTIC'];
+        let gCurr = container['boxCurrentWeight']['GLASS'];
+
+        let trolley = container['currState'];
+        let position = container['currPosition'];
+
+        let pMax = container['boxMaxWeight']['PLASTIC'];
+        let gMax = container['boxMaxWeight']['GLASS'];
+
+        let led = container['currLedState'];
+
+        setMessageToWindow(glassprog, gCurr.toString());
+        setMessageToWindow(glassMax, gMax.toString());
+
+
+        setMessageToWindow(plasticprog, pCurr.toString());
+        setMessageToWindow(plasticMax, pMax.toString());
+
+        setMessageToWindow(trolleystate, trolley.toString());
+        setMessageToWindow(trolleyposition, position.toString());
+
+        setMessageToWindow(ledstate, led.toString());
+
+        let div = document.getElementById("table");
+
+        if (position.toString() == "HOME"){
+            div.innerHTML = ""
+            generateTableHome();
+        }
+
+        if (position.toString() == "INDOOR"){
+            div.innerHTML = ""
+            generateTableIndoor();
+        }
+
+        if (position.toString() == "PLASTICBOX"){
+            div.innerHTML = ""
+            generateTablePlastic();
+        }
+        if(position.toString() == "GLASSBOX"){
+            div.innerHTML = ""
+            generateTableGlass();
+        }
+    };
+
+}//connect
+
--- Sprint2/unibo.webgui/src/main/resources/static/table.js
+++ Sprint2/unibo.webgui/src/main/resources/static/table.js
@@ -1,16 +1,9 @@
-/*
-table.js
-*/
-
-const home = [0, 0]
-const indoor = [0, 8]
-const plastic = [10, 8]
-const glass = [10, 0]
+const home = [0,0]
+const indoor = [0,8]
+const plastic = [10,8]
+const glass = [10,0]
 
 function generateTable() {
-    console.log("document")
-}
-    /*
     let table = document.createElement("table")
     table.classList.add("table")
     table.classList.add("table-bordered")
@@ -21,15 +14,106 @@ function generateTable() {
         tr.style = "height: 40px;"
         for (let j = 0; j < plastic[0]; j++) {
             let td = document.createElement("td")
-            td.style = "width: 40px; border:1px solid; background: #808080; opacity: 0.5; text-align: center; vertical-align: middle;"
+            td.style = "width: 40px; border:1px solid; background: #E3F2FD; text-align: center; vertical-align: middle;"
             tr.appendChild(td)
         }
         tbody.appendChild(tr)
     }
     let div = document.getElementById("table")
-    div.appendChild(table)
+    div.appendChild(table);
+}
 
-   */
+function generateTableHome(){
+    let table = document.createElement("table")
+    table.classList.add("table")
+    table.classList.add("table-bordered")
+    let tbody = document.createElement("tbody")
+    table.appendChild(tbody)
+    for (let i = 0; i < indoor[1]; i++) {
+        let tr = document.createElement("tr")
+        tr.style = "height: 40px;";
+        for (let j = 0; j < plastic[0]; j++) {
+            let td=document.createElement("td")
+            td.style="width: 40px; border:1px solid; background: #E3F2FD; opacity: 0.5; text-align: center; vertical-align: middle;"
+            if(i==home[0] && j==home[1]) {
+                td.style = "width:40px; border:1px solid; background-image: radial-gradient(circle 8px, white 99%, grey);"
+            }
+            tr.appendChild(td)
+        }
+        tbody.appendChild(tr)
+    }
+    let div = document.getElementById("table")
+    div.appendChild(table);
+}
 
+function generateTableIndoor(){
+    let table = document.createElement("table")
+    table.classList.add("table")
+    table.classList.add("table-bordered")
+    let tbody = document.createElement("tbody")
+    table.appendChild(tbody)
+    for (let i = 0; i < indoor[1]; i++) {
+        let tr = document.createElement("tr")
+        tr.style = "height: 40px;";
+        for (let j = 0; j < plastic[0]; j++) {
+            let td=document.createElement("td")
+            td.style="width: 40px; border:1px solid; background: #E3F2FD; opacity: 0.5; text-align: center; vertical-align: middle;"
+            if(i==indoor[1]-1 && j==indoor[0]) {
+                td.style = "width:40px; border:1px solid; background-image: radial-gradient(circle 8px, white 99%, blue);"
+            }
+            tr.appendChild(td)
+        }
 
+        tbody.appendChild(tr)
+    }
+    let div = document.getElementById("table")
+    div.appendChild(table);
+}
 
+function generateTablePlastic(){
+    let table = document.createElement("table")
+    table.classList.add("table")
+    table.classList.add("table-bordered")
+    let tbody = document.createElement("tbody")
+    table.appendChild(tbody)
+    for (let i = 0; i < indoor[1]; i++) {
+        let tr = document.createElement("tr")
+        tr.style = "height: 40px;";
+        for (let j = 0; j < plastic[0]; j++) {
+            let td=document.createElement("td")
+            td.style="width: 40px; border:1px solid; background: #E3F2FD; opacity: 0.5; text-align: center; vertical-align: middle;"
+            if(i==plastic[1]-1 && j==plastic[0]-1) {
+                td.style = "width:40px; border:1px solid; background-image: radial-gradient(circle 8px, white 99%, yellow);"
+            }
+            tr.appendChild(td)
+        }
+
+        tbody.appendChild(tr)
+    }
+    let div = document.getElementById("table")
+    div.appendChild(table);
+}
+
+function generateTableGlass(){
+    let table = document.createElement("table")
+    table.classList.add("table")
+    table.classList.add("table-bordered")
+    let tbody = document.createElement("tbody")
+    table.appendChild(tbody)
+    for (let i = 0; i < indoor[1]; i++) {
+        let tr = document.createElement("tr")
+        tr.style = "height: 40px;";
+        for (let j = 0; j < plastic[0]; j++) {
+            let td=document.createElement("td")
+            td.style="width: 40px; border:1px solid; background: #E3F2FD; opacity: 0.5; text-align: center; vertical-align: middle;"
+            if(i==glass[1] && j==glass[0]-1) {
+                td.style = "width:40px; border:1px solid; background-image: radial-gradient(circle 8px, white 99%, green);"
+            }
+            tr.appendChild(td)
+        }
+
+        tbody.appendChild(tr)
+    }
+    let div = document.getElementById("table")
+    div.appendChild(table);
+}
