53be4c243e2c5421efb09207d6a7012852c10ff1
annamaria <annamaria.simonitto@studio.unibo.it>
2022-11-04 15:03:29 +0100

    Sprint3_ProblemAnalysis1.0

---
+++ Sprint3/unibo.led/Arch_raspberry.py
@@ -0,0 +1,28 @@
+from diagrams import Cluster, Diagram, Edge
+from diagrams.custom import Custom
+import os
+os.environ['PATH'] += os.pathsep + 'C:/Program Files/Graphviz/bin/'
+
+graphattr = {     #https://www.graphviz.org/doc/info/attrs.html
+    'fontsize': '22',
+}
+
+nodeattr = {   
+    'fontsize': '22',
+    'bgcolor': 'lightyellow'
+}
+
+eventedgeattr = {
+    'color': 'red',
+    'style': 'dotted'
+}
+with Diagram('raspberryArch', show=False, outformat='png', graph_attr=graphattr) as diag:
+  with Cluster('env'):
+     sys = Custom('','./qakicons/system.png')
+     with Cluster('ctxwasteservice', graph_attr=nodeattr):
+          systemstatemanager=Custom('systemstatemanager(ext)','./qakicons/externalQActor.png')
+     with Cluster('ctxpi', graph_attr=nodeattr):
+          led=Custom('led','./qakicons/symActorSmall.png')
+          sonar=Custom('sonar','./qakicons/symActorSmall.png')
+     led >> Edge(color='blue', style='solid', xlabel='updateled', fontcolor='blue') >> systemstatemanager
+diag
---
+++ Sprint3/unibo.led/raspberry.pl
@@ -0,0 +1,8 @@
+%====================================================================================
+% raspberry description   
+%====================================================================================
+context(ctxwasteservice, "127.0.0.1",  "TCP", "8055").
+context(ctxpi, "localhost",  "TCP", "8090").
+ qactor( systemstatemanager, ctxwasteservice, "external").
+  qactor( led, ctxpi, "it.unibo.led.Led").
+  qactor( sonar, ctxpi, "it.unibo.sonar.Sonar").
