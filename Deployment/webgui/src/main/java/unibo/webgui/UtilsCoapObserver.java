6f221581b9826b93288199b463d18582ded4d663
Alessio Reitano <alessio.reitano@studio.unibo.it>
2022-11-14 11:27:39 +0100

    Sprint_Deployment2.0

---
+++ Deployment/webgui/src/main/java/unibo/webgui/UtilsCoapObserver.java
@@ -0,0 +1,19 @@
+package unibo.webgui;
+
+import org.eclipse.californium.core.CoapHandler;
+import org.eclipse.californium.core.CoapResponse;
+import unibo.comm22.utils.ColorsOut;
+
+public class UtilsCoapObserver implements CoapHandler {
+
+    @Override
+    public void onLoad(CoapResponse response) {
+        ColorsOut.outappl("UtilsCoapObserver changed!" + response.getResponseText(), ColorsOut.GREEN);
+        WebSocketConfiguration.wshandler.sendToAll(""+response.getResponseText());
+    }
+
+    @Override
+    public void onError() {
+        ColorsOut.outerr("UtilsCoapObserver observe error!");
+    }
+}
