7a910d2b31942f4cab9057bf37d8e77af850a070
Alessio Reitano <alessio.reitano@studio.unibo.it>
2022-11-28 17:42:37 +0100

    Deployment_3.0

---
+++ Deployment/unibo.wasteService/wasteservice.pl
@@ -0,0 +1,14 @@
+%====================================================================================
+% wasteservice description   
+%====================================================================================
+context(ctxbasicrobot, "robot",  "TCP", "8020").
+context(ctxpi, "raspberrypi.local",  "TCP", "8065").
+context(ctxwasteservice, "localhost",  "TCP", "8055").
+ qactor( led, ctxpi, "external").
+  qactor( pathexec, ctxbasicrobot, "external").
+  qactor( wasteservice, ctxwasteservice, "it.unibo.wasteservice.Wasteservice").
+  qactor( transporttrolleycore, ctxwasteservice, "it.unibo.transporttrolleycore.Transporttrolleycore").
+  qactor( transporttrolleymover, ctxwasteservice, "it.unibo.transporttrolleymover.Transporttrolleymover").
+  qactor( transporttrolleyexecutor, ctxwasteservice, "it.unibo.transporttrolleyexecutor.Transporttrolleyexecutor").
+  qactor( systemstatemanager, ctxwasteservice, "it.unibo.systemstatemanager.Systemstatemanager").
+  qactor( sonarfilter, ctxwasteservice, "it.unibo.sonarfilter.Sonarfilter").
